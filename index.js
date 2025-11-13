0b3f6df4572ce80d2086428f5bd0fa5a77c8a32b
takahiroaoki917-ops <takahiroaoki917@gmail.com>
2025-11-13 23:23:13 +0900

    Update index.js

--- index.js
+++ index.js
@@ -20,3 +20,11 @@ const generatedPasswordOne = generateRandomPassword()
 
 console.log("Here is a random password: ", generatedPasswordOne)
 
+const generateBtn = document.getElementById("generate-btn")
+
+generateBtn.addEventListener("click", function() {
+    const passwordOne = document.getElementById("password-one")
+    const passwordTwo = document.getElementById("password-two")
+    passwordOne.textContent = generateRandomPassword()
+    passwordTwo.textContent = generateRandomPassword()
+}
